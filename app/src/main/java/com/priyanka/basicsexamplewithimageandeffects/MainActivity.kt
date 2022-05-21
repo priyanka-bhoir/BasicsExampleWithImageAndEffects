c4033c2415842f34fc13c0a56deacebf33b3df0d
priyanka-bhoir <er.priyankabhoir1@gmail.com>
2022-05-21 12:41:54 +0530

    adding the image

--- app/src/main/java/com/priyanka/basicsexamplewithimageandeffects/MainActivity.kt
+++ app/src/main/java/com/priyanka/basicsexamplewithimageandeffects/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.priyanka.basicsexamplewithimageandeffects
 
+import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.priyanka.basicsexamplewithimageandeffects.ui.theme.BasicsExampleWithImageAndEffectsTheme
+import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +36,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val painter = painterResource(id = R.drawable.image)
+                    val title = "PRiyanka"
+                    val desc = "the Great Priyanka, She is the Best...!"
+                    ImageCard(painter,title,desc)
                 }
             }
         }
@@ -30,14 +47,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun ImageCard(
+    painter: Painter,
+    contentDescription : String,
+    title : String,
+    modifier: Modifier = Modifier
+) {
+    Card(modifier.fillMaxWidth(),
+    shape = RoundedCornerShape(15.dp),
+    elevation = 5.dp){
+        Box(Modifier.height(200.dp)){
+            Image(painter = painter,
+                contentDescription = contentDescription,
+            contentScale = ContentScale.Crop)
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp)
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f
+                    )
+                ),
+                contentAlignment =  Alignment.BottomStart){
+                Text(text = title,
+                    style = androidx.compose.ui.text.TextStyle(Color.White,16.sp))
+            }
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BasicsExampleWithImageAndEffectsTheme {
-        Greeting("Android")
     }
 }
+
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    BasicsExampleWithImageAndEffectsTheme {
+//        Greeting("Android")
+//    }
+//}
